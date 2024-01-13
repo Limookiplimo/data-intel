a234f4bc33e87b707ac00ed7e350b6820b2cff2e
limoo <limoodrice@gmail.com>
2024-01-13 14:41:03 +0300

    Added a fucntion to calculate kep performance indicators.

--- dataviz/viz.py
+++ dataviz/viz.py
@@ -33,11 +33,35 @@ def create_pandas_dataframe():
     data = get_data()
     cols = ["crm","invoice_number","invoice_date","total_price","total_weight","payment_amount","payment_date","phone","route","rep_name"]
     df = pd.DataFrame(data, columns=cols)
-    sales_by_route = df.groupby('route')['total_price'].sum()
-    st.write(sales_by_route)
+    return df
+
+@st.cache_data
+def calculate_kpis():
+    df = create_pandas_dataframe()
+    total_sales = int(df["total_price"].sum())
+    total_invoices = df["invoice_number"].count()
+    total_payments = int(df["payment_amount"].sum())
+    invoice_totals = df.groupby("invoice_number")[["total_price", "payment_amount"]].sum()
+    pending_payments = int((invoice_totals["total_price"] - invoice_totals["payment_amount"]).sum())
+    tonnage = int(df["total_weight"].sum())
+    l1, l2, m, r1, r2 = st.columns(5)
+    with l1:
+        st.subheader("Total Sales:")
+        st.subheader(f"{total_sales:,}")
+    with l2:
+        st.subheader("Total Payments:")
+        st.subheader(f"{total_payments:,}")
+    with r1:
+        st.subheader("Total Tonnage:")
+        st.subheader(f"{tonnage:,}")
+    with r2:
+        st.subheader("Pending Payments:")
+        st.subheader(f"{pending_payments:,}")
+    with m:
+        st.subheader("Total Invoices:")
+        st.subheader(f"{total_invoices:,}")
+    
+    st.markdown("---")
 
 if __name__ == "__main__":
-    results = create_pandas_dataframe()
-    results
-
-
+    calculate_kpis()
