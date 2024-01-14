2fe16b34c03cd003bf7ce27b0912d7722fb91b42
limoo <limoodrice@gmail.com>
2024-01-14 17:20:51 +0300

    Optimized sql query for fetching from the database

--- dataviz/viz.py
+++ dataviz/viz.py
@@ -15,18 +15,18 @@ st.title("Retail Analytics")
 def connect_db():
     return psycopg2.connect(**DB_CONFIG)
 
-@st.cache_data
 def get_data():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""select s.crm, s.invoice_number, s.invoice_date, s.total_price, s.total_weight, 
-                           payment_amount, payment_date, phone, m.route, rep_name
-                from sales s
-                join accounts a on a.invoice_number = s.invoice_number
-                join customers c on c.crm  = s.crm 
-                join deliveries d on d.invoice_number = s.invoice_number 
-                join markets m on m.route = c.route
-                join sales_reps sr on sr.market = m.name""")
+            cur.execute("""
+                        select s.crm, s.invoice_number, s.invoice_date, s.total_price, s.total_weight, 
+                            payment_amount, payment_date, phone, m.route, rep_name
+                        from sales s
+                        join accounts a on a.invoice_number = s.invoice_number
+                        join customers c on c.crm  = s.crm 
+                        join deliveries d on d.invoice_number = s.invoice_number 
+                        join markets m on m.route = c.route
+                        join sales_reps sr on sr.market = m.name""")
             return cur.fetchall()
 
 def create_pandas_dataframe():
