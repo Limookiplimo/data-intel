5549c2035fb067ebda0f63aeb99294c9e42d3fbf
limoo <limoodrice@gmail.com>
2024-01-17 18:47:35 +0300

    first commit

---
+++ dataviz/pages/Accounts.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import plotly.express as px
+from Overview import create_pandas_dataframe
+st.set_page_config(layout="wide")
+st.markdown("<h2 style='text-align: center; color: green'> ABC Acoounts</h1>", unsafe_allow_html=True)
