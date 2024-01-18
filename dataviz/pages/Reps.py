a5e3cc4b84210b538bce3f420f128ad5e1adc5db
limoo <limoodrice@gmail.com>
2024-01-18 11:27:44 +0300

    First commit

--- dataviz/pages/Reps.py
+++ dataviz/pages/Reps.py
@@ -2,4 +2,5 @@ import streamlit as st
 import plotly.express as px
 from Overview import create_pandas_dataframe
 st.set_page_config(layout="wide")
-st.markdown("<h2 style='text-align: center; color: green'> Sales Representatives</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: green'> ABC Sales Representatives</h1>", unsafe_allow_html=True)
+
