df3d8e06d8cc991785c110a8996984ad3d8280cd
limoo <limoodrice@gmail.com>
2024-01-24 18:04:49 +0300

    Updated routes charts titles and variables. Refactored code structure by removing blank lines and spaces

--- dataviz/pages/Routes.py
+++ dataviz/pages/Routes.py
@@ -10,18 +10,18 @@ def sales_per_route_chart():
     estimated_value = 150000000
     total_sales = int(df["total_price"].sum())
     route_sales_monthly = df.groupby(["route","inv_month"],as_index=False)["total_price"].sum()
+    route_sales_monthly.columns = ["Routes","Month","Sales"]
     left_chart, right_chart = st.columns(2)
     with left_chart:
         st.header(":blue[Route KPIs]")
-        st.subheader(f"Route Value: $ {estimated_value:,}")
-
-        st.subheader(f"Total Sales: $ {total_sales:,}")
-
-        st.subheader(f"Total Shops: {total_customers:,}")
+        st.subheader(f"Est. Value: $ {estimated_value:,}")
+        st.subheader(f"Sales: $ {total_sales:,}")
+        st.subheader(f"Customers: {total_customers:,}")
 
     with right_chart:
-        fig_monthly_sales = px.line(route_sales_monthly, x="inv_month", y="total_price", title="Monthly Sales", color="route", line_shape="linear", render_mode="svg")
+        fig_monthly_sales = px.line(route_sales_monthly,x="Month",y="Sales",title="Sales per Month",color="Routes",line_shape="linear",render_mode="svg")
         st.plotly_chart(fig_monthly_sales)
+
     st.markdown("---")
     
 def monthly_sales_and_top_customers_chart():
@@ -30,12 +30,14 @@ def monthly_sales_and_top_customers_chart():
     top_customers = df.groupby("crm")[["total_price"]].sum().nlargest(10, "total_price")
     left_chart, right_chart = st.columns(2)
     with left_chart:
-        fig_total_sales = px.bar(route_sales,x=route_sales.index,y="total_price",color='total_price',labels={'total_price':'Total Sales'},title="Trading Routes")
+        fig_total_sales = px.bar(route_sales,x=route_sales.index,y="total_price",color="total_price",labels={"total_price":"Total Sales"},title="Sales per Trading Routes")
         fig_total_sales.update_xaxes(title_text="Route")
+        fig_total_sales.update_yaxes(title_text="Amount")
         st.plotly_chart(fig_total_sales)
     with right_chart:
-        fig_top_customers = px.bar(top_customers,x=top_customers.index,y="total_price",color="total_price",labels={"total_price": "Total Sales"},title="Top Customers")
+        fig_top_customers = px.bar(top_customers,x=top_customers.index,y="total_price",color="total_price",labels={"total_price": "Total Sales"},title="Top Valuable Customers")
         fig_top_customers.update_xaxes(title_text="Customer")
+        fig_top_customers.update_yaxes(title_text="Amount")
         st.plotly_chart(fig_top_customers)
 
 if __name__ == "__main__":
